6783197d7d90be526af784ab78ea2f1a98653856
Babatunde <babatundea15@gmail.com>
2019-11-13 06:18:21 +0100

    MVP reached

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -49,7 +49,7 @@ const Movie = props => {
             </div>
           ))}
       </div>
-      <div className="save-button">Save</div>
+      <button onClick={() => props.addMovie(movie)} className="save-button">Save</button>
     </div>
   );
 };
